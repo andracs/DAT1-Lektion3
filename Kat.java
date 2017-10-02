1ff85a230377b66588c64bb99dceb0ad152c04bc
Burhozturk <31951806+Burhozturk@users.noreply.github.com>
2017-10-02 12:07:29 +0200

    Add files via upload

---
+++ Kat.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kat;
+
+
+
+/* En klasse er en slags opskrift på, hvordan vi laver katte */
+
+public class Kat {
+
+
+
+    /* DATA FIELDS - Klassens data fields */
+
+    int alder = -100;
+
+    String navn = "NONAME";
+
+
+
+    /* KONSTRUKTOR
+
+     * Jubii, constructoren kaldes hver gang, der laves et nyt objekt.
+
+     * Vi har besluttet, at alle katte SKAL have et navn ved oprettelsen
+
+     * Derfor er der ingen Kat() konstruktor uden et navn som argument */
+
+    Kat(String kattensNavn) {
+
+        navn = kattensNavn;
+
+        System.out.println("Ny kat er avlet: " + navn);
+
+    }
+
+
+
+    /* METODER - Her er de ting, som katterne kan gøre */
+
+    public void hop() {
+
+        System.out.println(navn + " hopper.");
+
+    }
+
+
+
+    public void sov() {
+
+        System.out.println(navn + " sover.");
+
+    }
+
+
+
+    public void setAlder(int kattensAlder) {
+
+        alder = kattensAlder;
+
+        System.out.println("Kattens har nu fået sin alder oprettet.");
+
+    }
+
+
+
+    public void hvorGammel() {
+
+        System.out.println(navn + " er " + alder + " år gammel.");
+
+    }
+
+
+
+    private void breath() {
+
+        System.out.println("Katten trækker vejret.");
+
+    }
+
+
+
+}
